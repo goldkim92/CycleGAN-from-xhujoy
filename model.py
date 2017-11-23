06d9f755ed6bd178c397b81bf58e07df43336e6c
goldkim92 <goldkim92@gmail.com>
2017-11-23 10:56:24 +0900

    annotate the printing variable names

--- model.py
+++ model.py
@@ -129,7 +129,7 @@ class cyclegan(object):
         t_vars = tf.trainable_variables()
         self.d_vars = [var for var in t_vars if 'discriminator' in var.name]
         self.g_vars = [var for var in t_vars if 'generator' in var.name]
-        for var in t_vars: print(var.name)
+#        for var in t_vars: print(var.name)
 
     def train(self, args):
         """Train cyclegan"""
